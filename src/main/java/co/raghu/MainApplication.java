537d3029011fbba62eb1a8e4c90cb3f3b6ad1d86
Raghu <raghuprasad.gubbi@gmail.com>
2017-09-17 13:39:33 +0800

    Helloworld program

---
+++ src/main/java/co/raghu/MainApplication.java
@@ -0,0 +1,20 @@
+package co.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
